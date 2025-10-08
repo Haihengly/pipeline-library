04a9bcf6384d1841a757011cf2fec79efed66d8f
Haihengly <your-email@example.com>
2025-10-08 16:19:18 +0700

    custom container name use -p

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,21 @@
 def call(Map config) { 
     def STORE_DIR = "/My-Docker/Dev-Service"
-    def envFile = config.envName == "main" ? ".env.main" : ".env.dev"
-    def composeFiles = "-f docker-compose.base.yml -f docker-compose.${config.envName}.yml"
+    def PROJECT_NAME = env.JOB_NAME.tokenize('/')[0]
+    def envFile
+        switch(config.envName) {
+            case "main":
+                envFile = ".env.main"
+                break
+            case "dev":
+                envFile = ".env.dev"
+                break
+            case "test":
+                envFile = ".env.test"
+                break
+            default:
+                error "Unknown environment: ${config.envName}"
+        }
+    def composeFiles = "-p ${PROJECT_NAME}-${config.envName} -f docker-compose.base.yml -f docker-compose.${config.envName}.yml"
 
     echo "Building Docker images with docker-compose..."
     sh """
--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,20 @@
 def call(Map config) {
     def STORE_DIR = "/My-Docker/Dev-Service"
-    def envFile = config.envName == "main" ? ".env.main" : ".env.dev"
-    def composeFiles = "-f docker-compose.base.yml -f docker-compose.${config.envName}.yml"
+    def envFile
+        switch(config.envName) {
+            case "main":
+                envFile = ".env.main"
+                break
+            case "dev":
+                envFile = ".env.dev"
+                break
+            case "test":
+                envFile = ".env.test"
+                break
+            default:
+                error "Unknown environment: ${config.envName}"
+        }
+    def composeFiles = "-p ${PROJECT_NAME}-${config.envName} -f docker-compose.base.yml -f docker-compose.${config.envName}.yml"
 
     echo 'Starting containers...'
     sh """
